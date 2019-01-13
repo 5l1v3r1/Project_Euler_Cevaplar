c4d72a58f633d8230b2d5f26cd2d9ba98f44818b
ebruakdemir <45771338+ebruakdemir@users.noreply.github.com>
2019-01-13 21:42:00 +0300

    Update Problem2.java

--- java/Problem2.java
+++ java/Problem2.java
@@ -1,30 +1,30 @@
 public class Problem2 {
 	
 	static final long NUMBER = 4000000;
-	static long toplam = 0;
+	static long sum = 0;
 	static long t = 0;
 	static long t1 = 0;
 	static long t2 = 1;
 
 	public static void main(String[] args) {
 		
-		System.out.println(fibonacciToplam());
+		System.out.println(fibonacciSum());
 
 	}
 
-	private static long fibonacciToplam() {
+	private static long fibonacciSum() {
 			
 		t = t1 + t2;
 		t1 = t2;
 		t2 = t;
 		
 		if(t1>=NUMBER)
-			return toplam;
+			return sum;
 		else {
 			if(t1%2 == 0)
-				return toplam = t1 + fibonacciToplam();
+				return sum = t1 + fibonacciSum();
 			else {
-				return fibonacciToplam();
+				return fibonacciSum();
 			}
 		}
 		
